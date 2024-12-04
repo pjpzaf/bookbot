800adebb362281eaf4c1422778cc18ba0d22a01e
root <root@ZAF-PJP.>
2024-12-04 12:41:16 +0800

    added character count feature

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        return print(file_contents)
+        word_list = file_contents.lower().split()
+        character_list = []
+        character_count = {}
+        temp_var = ""
+        for word in word_list:
+            temp_var = list(word)
+            character_list += temp_var
+        for character in character_list:
+            if character in character_count:
+                character_count[character] += 1
+            else:
+                character_count[character] = 1
+        print(character_count)
+    
 
 if __name__=="__main__":
     main()
